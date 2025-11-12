96766a91a81107a71cabb511707a4cd5b304a678
atheeq-rhxn <atheeq.rhxn@gmail.com>
2025-11-12 19:20:38 +0530

    20251112192038

--- config/mango/scripts/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "up" ]; then
-    wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+
-elif [ "$1" = "down" ]; then
-    wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-
-fi
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,22 +1,9 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function(cmd)
-	if mux then
-  	local tab, pane, window = mux.spawn_window(cmd or {})
-  	window:gui_window():maximize()
-	end
-end)
-
 local config = {}
 
-if wezterm.config_builder then config = wezterm.config_builder() end
-
-config.front_end = "WebGpu"
-config.max_fps = 120
-config.animation_fps = 120
-config.cursor_blink_rate = 250
-config.default_prog = { "/home/atheeq/.cargo/bin/zellij" }
+config.default_prog = { "zellij" }
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 0.7
