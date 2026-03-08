9e3346149b175fb3c856db1f3758d3ef4ed52165
atheeq-rhxn <atheeq.rhxn@gmail.com>
2026-03-08 16:23:57 +0530

    20260308162357

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -2,8 +2,6 @@ require("full-border"):setup()
 require("folder-rules"):setup()
 require("no-status"):setup()
 
-local old_layout = Tab.layout
-
 if Header.redraw then
 	Header.redraw = function()
 		return {}
@@ -14,6 +12,7 @@ else
 	end
 end
 
+local old_layout = Tab.layout
 Tab.layout = function(self, ...)
 	self._area = ui.Rect({ x = self._area.x, y = self._area.y - 1, w = self._area.w, h = self._area.h + 1 })
 	return old_layout(self, ...)
